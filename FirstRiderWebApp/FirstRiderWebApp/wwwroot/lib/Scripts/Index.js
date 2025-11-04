c857a58af9615fc5d2c9ad26b86ddbecfe3fdcb8
Emil Berg <emil94berg@gmail.com>
2025-11-04 14:00:27 +0100

    add new package

--- FirstRiderWebApp/FirstRiderWebApp/wwwroot/lib/Scripts/Index.js
+++ FirstRiderWebApp/FirstRiderWebApp/wwwroot/lib/Scripts/Index.js
@@ -1,7 +1,12 @@
 ﻿import ko from "../knockoutjs/koIndex.js";
 function TestViewModel(){
+    const viewModel = this; 
     this.name = ko.observable('');
+    this.lastName = ko.observable('');
     this.age = ko.observable(0);
+    this.fullname = ko.computed( () => {
+        return viewModel.name() + ' ' + viewModel.lastName();
+    });
 }
 
 ko.applyBindings(new TestViewModel());
