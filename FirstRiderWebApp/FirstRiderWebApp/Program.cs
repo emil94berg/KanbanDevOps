281d81bf2808943decb669bf8ab1dd481b03be1c
Emil Berg <emil94berg@gmail.com>
2025-11-04 15:44:22 +0100

    Setup dbcontext

--- FirstRiderWebApp/FirstRiderWebApp/Program.cs
+++ FirstRiderWebApp/FirstRiderWebApp/Program.cs
@@ -1,9 +1,18 @@
+using System.Reflection;
+using FirstRiderWebApp.wwwroot.lib.DAL;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
 var app = builder.Build();
 
 // Kollar automatiskt efter index.html (och default.html etc.)
 app.UseDefaultFiles(); 
-
 // Serverar statiska filer från wwwroot
 app.UseStaticFiles();
 
---
+++ FirstRiderWebApp/FirstRiderWebApp/wwwroot/lib/DAL/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FirstRiderWebApp.wwwroot.lib.DAL;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+    public DbSet<Model.Kanban> Kanban { get; set; }   
+    public DbSet<Model.Column> Column { get; set; }   
+    public DbSet<Model.Card> Card { get; set; }   
+}
